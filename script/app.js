959e97c66e55bf0ea41ee1436e777efc946d8000
tasib94 <rahi84974@gmail.com>
2024-02-21 18:08:19 +0600

    add common class to every seat

--- script/app.js
+++ script/app.js
@@ -1,8 +1,18 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     const buyTicketButton = document.getElementById('buy-ticket');
     const selectTicketSection = document.getElementById('select-your-seat');
 
-    buyTicketButton.addEventListener('click',function(){
+    buyTicketButton.addEventListener('click', function () {
         selectTicketSection.scrollIntoView({ behavior: 'smooth' });
     });
 });
+
+const allSeat = document.getElementsByClassName('seat');
+let count = 24;
+
+for (const seat of allSeat) {
+    seat.addEventListener('click', function (e) {
+        count = count - 1;
+        document.getElementById('seats-left').innerText = count;
+    });
+}
