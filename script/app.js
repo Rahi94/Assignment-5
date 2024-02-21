670c6e5dafc2e00518b2f6d5cfa360e272bf4050
tasib94 <rahi84974@gmail.com>
2024-02-21 22:50:49 +0600

    added the handleclick evenlistener by adding seat and price

--- script/app.js
+++ script/app.js
@@ -9,10 +9,35 @@ document.addEventListener('DOMContentLoaded', function () {
 
 const allSeat = document.getElementsByClassName('seat');
 let count = 24;
+let seatCount = 0;
 
 for (const seat of allSeat) {
     seat.addEventListener('click', function (e) {
+        seat.classList.add('bg-green-400');
         count = count - 1;
         document.getElementById('seats-left').innerText = count;
+        seatCount = seatCount + 1;
+        document.getElementById('seat-count').innerText = seatCount;
+        // create element
+        const appendDiv = document.getElementById('append-div');
+
+        const createDiv = document.createElement('div')
+        const createSeat = document.createElement('p')
+        const post = document.createElement('p');
+        post.innerText = 'Economy';
+        const price = document.createElement('p');
+        price.innerText = '550';
+        createSeat.setAttribute('class', 'text-xl',);
+        createSeat.innerText = seat.innerText;
+        
+
+        createDiv.appendChild(createSeat);
+        createDiv.appendChild(post);
+        createDiv.appendChild(price);
+
+        createDiv.classList.add('flex', 'justify-between', )
+
+        appendDiv.appendChild(createDiv);
+
     });
 }
